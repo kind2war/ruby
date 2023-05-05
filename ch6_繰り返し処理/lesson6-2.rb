d0893324d55bc9a0036e1ee18e4baa56c27d60df
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-05 08:04:04 +0000

    [fin]回数表示の処理と、繰り返し処理

--- ch6_繰り返し処理/lesson6-2.rb
+++ ch6_繰り返し処理/lesson6-2.rb
@@ -1,23 +1,24 @@
 puts "計算をはじめます"
 puts "何回計算を繰り返しますか？"
-
 n = gets.to_i
+
 k = 0
 while k <= n do
-    puts "#{k+1}回目の計算"
-    puts "２つの値を入力してください"
-      num1 = gets.to_i
-      num2 = gets.to_i
-    puts "a=#{num1}"
-    puts "b=#{num2}"
-    puts "計算結果を出力します"
-      puts "#{num1}+#{num2}=#{num1 + num2}"
-      puts "#{num1}-#{num2}=#{num1 - num2}"
-      puts "#{num1}*#{num2}=#{num1 * num2}"
-      puts "#{num1}/#{num2}=#{num1 / num2...num1%num2}"
-      puts k += 1
+  puts "#{k+1}回目の計算"
+  puts "２つの値を入力してください"
+    num1 = gets.to_i
+    num2 = gets.to_i
+  puts "a=#{num1}"
+  puts "b=#{num2}"
+  puts "計算結果を出力します"
+  puts "#{num1}+#{num2}=#{num1 + num2}"
+  puts "#{num1}-#{num2}=#{num1 - num2}"
+  puts "#{num1}*#{num2}=#{num1 * num2}"
+  puts "#{num1}/#{num2}=#{num1 / num2...num1%num2}"
+k += 1
+
   if k == n
-   puts "計算を終了します"
+    puts "計算を終了します"
   break
   end
 end
